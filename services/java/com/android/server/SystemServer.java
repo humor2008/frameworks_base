349d00ec31b03c5f1f627a47b0daad3da937d74a
Ethan Chen <intervigil@gmail.com>
2013-10-08 15:20:02 -0700

    Support GESTURE_SENSOR input device type with GestureService
    * The GESTURE_SENSOR input device type is meant to support touch sensors
      which are meant for gesture input only, very similar to a touchpad,
      but without the pointer capability.
    * Define separate service to handle gestures from GESTURE_SENSOR device
      type.
    
    Change-Id: I9b273df2a3cc141774d7f7cd81e43a90ea5b230b
    
    Hide InputDevice.SOURCE_GESTURE_SENSOR from API
    
    Change-Id: If009e9595fc593594b0e7764669996de137483a1
    
    GestureInput : Allow doubletap/longpress configuration
    
    Allows devices to specify pending intents for double tap and
    long press events.
    
    Change-Id: I7e7cc2f9f96a01d8f6232e5cf0e19832fdfd5359
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	core/res/res/values/symbols.xml
    
    Conflicts:
    	core/res/res/values/symbols.xml
    
    Conflicts:
    	core/res/res/values/symbols.xml
    
    Conflicts:
    	core/res/res/values/symbols.xml
    
    Conflicts:
    	Android.mk
    	core/java/android/view/InputDevice.java
    	core/res/res/values/symbols.xml
    	services/java/com/android/server/SystemServer.java

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -70,6 +70,7 @@ import com.android.server.display.DisplayManagerService;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.fingerprint.FingerprintService;
 import com.android.server.hdmi.HdmiControlService;
+import com.android.server.gesture.GestureService;
 import com.android.server.input.InputManagerService;
 import com.android.server.job.JobSchedulerService;
 import com.android.server.lights.LightsService;
@@ -567,6 +568,7 @@ public final class SystemServer {
         AssetAtlasService atlas = null;
         MediaRouterService mediaRouter = null;
         ThemeService themeService = null;
+        GestureService gestureService = null;
         EdgeGestureService edgeGestureService = null;
 
         // Bring up services needed for UI.
@@ -975,6 +977,17 @@ public final class SystemServer {
                         new GraphicsStatsService(context));
             }
 
+            if (context.getResources().getBoolean(
+                    com.android.internal.R.bool.config_enableGestureService)) {
+                try {
+                    Slog.i(TAG, "Gesture Sensor Service");
+                    gestureService = new GestureService(context, inputManager);
+                    ServiceManager.addService("gesture", gestureService);
+                } catch (Throwable e) {
+                    Slog.e(TAG, "Failure starting Gesture Sensor Service", e);
+                }
+            }
+
             if (mPackageManager.hasSystemFeature(PackageManager.FEATURE_PRINTING)) {
                 mSystemServiceManager.startService(PRINT_MANAGER_SERVICE_CLASS);
             }
@@ -1123,6 +1136,14 @@ public final class SystemServer {
             mDisplayManagerService.systemReady(safeMode, mOnlyCore);
         } catch (Throwable e) {
             reportWtf("making Display Manager Service ready", e);
+        }
+
+        if (gestureService != null) {
+            try {
+                gestureService.systemReady();
+            } catch (Throwable e) {
+                reportWtf("making Gesture Sensor Service ready", e);
+            }
         }
 
         if (edgeGestureService != null) {
