e8a6f67ef36693f9145e4e1e54f72407708759bd
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-11-29 06:34:41 +0000

    Reboot/Recovery QS Tile
    Change-Id: I2c7956b7201e2103420fc7254051141807024fc4
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/du_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Conflicts:
    	packages/SystemUI/res/values/custom_strings.xml

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -208,7 +208,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("headsup")) return R.string.quick_settings_heads_up_label;
         else if (spec.equals("sync")) return R.string.quick_settings_sync_label;
         else if (spec.equals("battery_saver")) return R.string.quick_settings_battery_saver;
->>>>>>> 83e2a79... Add Battery Saver QS Tile
+        else if (spec.equals("reboot")) return R.string.quick_settings_reboot_label;
         return 0;
     }
 
@@ -457,6 +457,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("headsup")) return R.drawable.ic_qs_heads_up_on;
             else if (mSpec.equals("sync")) return R.drawable.ic_qs_sync_on;
             else if (mSpec.equals("battery_saver")) return R.drawable.ic_qs_battery_saver_on;
+            else if (mSpec.equals("reboot")) return R.drawable.ic_qs_reboot;
             return R.drawable.android;
         }
 
