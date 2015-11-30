4346db33f63062f7a67d5ca1a09a561c1e39a3be
Alex Cruz <mazdarider23@gmail.com>
2015-11-30 03:37:18 +0000

    Fix theme issue with reboot dialog
    Change-Id: If8c49c9f55f215b3efc5c9b49a48820b258c83bb

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -199,7 +199,7 @@ public final class ShutdownThread extends Thread {
 
                 if ((advancedReboot == 1 && !locked) || advancedReboot == 2) {
                     // Include options in power menu for rebooting into recovery or bootloader
-                    sConfirmDialog = new AlertDialog.Builder(uiContext)
+                    sConfirmDialog = new AlertDialog.Builder(context, com.android.internal.R.style.Theme_Material_DayNight_Dialog_Alert)
                             .setTitle(titleResourceId)
                             .setItems(
                                     com.android.internal.R.array.shutdown_reboot_options,
