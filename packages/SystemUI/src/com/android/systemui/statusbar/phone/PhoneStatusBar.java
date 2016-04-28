db612f353368dea33fbe53e50bbf2fb201b9f928
Varun Date <date.varun123@gmail.com>
2016-04-28 20:43:32 +0530

    Pulse Bar Customizations [1/3]
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -639,9 +639,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 	    resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVBAR_BUTTON_COLOR),
                     false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.PULSE_CUSTOM_DIMEN),
-                    false, this, UserHandle.USER_ALL);
 		    update();
         }
 
@@ -724,9 +721,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 	   } else if (uri.equals(Settings.System.getUriFor(
                 Settings.System.NAVBAR_BUTTON_COLOR))) {
    	        mNavigationController.updateNavbarOverlay(getNavbarThemedResources());
-	   } else if (uri.equals(Settings.System.getUriFor(
-                Settings.System.PULSE_CUSTOM_DIMEN))) {
- 	        mNavigationController.updateNavbarOverlay(getNavbarThemedResources());
 		} 
             update();
         }
